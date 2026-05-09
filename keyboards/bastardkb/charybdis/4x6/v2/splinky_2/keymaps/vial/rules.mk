a701287c41a9e6f87b47e51db0a547afb2ccae28
KSP <karamin.stanislav@gmail.com>
2026-05-09 20:24:53 +0300

    feat(keymaps/vial): enable Vial/VIA and disable heavy features to save flash

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
@@ -1,25 +1,8 @@
-# Vial & VIA
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
-QMK_SETTINGS = yes
-
-# Features
-COMBO_ENABLE = yes
-ONE_SHOT_ENABLE = yes
-MOUSE_ENABLE = yes
-
-# RGB (Disabled to save flash & avoid driver schema errors on RP2040)
-RGB_MATRIX_ENABLE = no
-RGBLIGHT_ENABLE = no
-VIALRGB_ENABLE = no
-
-# Pointing Device & Split
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = pmw3360
-SPLIT_KEYBOARD = yes
-SERIAL_DRIVER = vendor
-
-# Storage & Bootloader
-EEPROM_DRIVER = vendor
-BOOTLOADER = rp2040
+QMK_SETTINGS = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+SPACE_CADET_ENABLE = no
+TAP_DANCE_ENABLE = no
