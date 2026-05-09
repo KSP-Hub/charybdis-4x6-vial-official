0145ddc6f51dd70fd188dfbc1452b4efc5c53a82
KSP <karamin.stanislav@gmail.com>
2026-05-09 19:08:28 +0300

    fix(keymaps/vial/rules.mk): add MCU=RP2040 and consolidate build flags

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
@@ -1,24 +1,27 @@
+# MCU & Bootloader (обязательно для RP2040 в этой версии QMK)
+MCU = RP2040
+BOOTLOADER = rp2040
+
 # Vial & VIA
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
+VIALRGB_ENABLE = no
 QMK_SETTINGS = yes
 
 # Features
 COMBO_ENABLE = yes
 ONE_SHOT_ENABLE = yes
 MOUSE_ENABLE = yes
-
-# RGB (Disabled to save flash/memory on RP2040)
-RGB_MATRIX_ENABLE = no
-RGBLIGHT_ENABLE = no
-VIALRGB_ENABLE = no
-
-# Pointing Device & Split
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = pmw3360
 SPLIT_KEYBOARD = yes
 
-# Storage & Bootloader
+# Pointing Device
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3360
+
+# Lighting (отключено для экономии места)
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+
+# EEPROM
 EEPROM_DRIVER = vendor
-BOOTLOADER = rp2040
