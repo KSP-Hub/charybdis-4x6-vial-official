99349b870ed2c02b5c907ec0a403a0f542961896
KSP <karamin.stanislav@gmail.com>
2026-05-09 20:13:15 +0300

    fix: resolve RP2040 schema, PMW3360 CS pin, and split serial config for 4x6 vial build

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,24 +1,25 @@
 #pragma once
 
-// Vial Configuration
+/* Vial Configuration */
 #define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
-// Split & Handedness
+/* Split & Handedness */
 #define MASTER_RIGHT
+#define SOFT_SERIAL_PIN GP1
 
-// Pointing Device (PMW3360)
+/* Pointing Device (PMW3360) */
 #define POINTING_DEVICE_RIGHT
 #define POINTING_DEVICE_ROTATION_90
-#define POINTING_DEVICE_CS_PIN GP14  // Критический пин для PMW3360
+#define PMW3360_CS_PIN GP14
 #define PMW3360_DPI 1600
 
-// RP2040 EEPROM & Bootloader
+/* RP2040 Specifics */
 #define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 
-// Tuning
+/* Tuning */
 #define NO_ACTION_ONESHOT
 #define TAPPING_TERM 200
--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
@@ -4,26 +4,26 @@ VIAL_ENABLE = yes
 VIAL_INSECURE = yes
 QMK_SETTINGS = no
 
-# RP2040 Bootloader & Storage
-BOOTLOADER = rp2040
-EEPROM_DRIVER = vendor
-
-# Split & Communication
-SPLIT_KEYBOARD = yes
-SERIAL_DRIVER = vendor
-
-# Pointing Device
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = pmw3360
-MOUSE_ENABLE = yes
-
-# RGB (Disabled to save flash & avoid schema/driver conflicts)
-RGB_MATRIX_ENABLE = no
-RGBLIGHT_ENABLE = no
-VIALRGB_ENABLE = no
-
-# Space Saving
+# Features (отключено для экономии flash и избежания конфликтов)
 COMBO_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
 SPACE_CADET_ENABLE = no
 TAP_DANCE_ENABLE = no
+ONE_SHOT_ENABLE = no
+MOUSE_ENABLE = yes
+
+# RGB (полностью отключено)
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+VIALRGB_ENABLE = no
+
+# Pointing Device
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3360
+
+# Split & RP2040
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+EEPROM_DRIVER = vendor
+BOARD = GENERIC_PROMICRO_RP2040
+BOOTLOADER = rp2040
