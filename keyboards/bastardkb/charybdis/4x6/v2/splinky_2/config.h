59c82f765fc69051e4f6f334932043d3297d7d81
KSP <karamin.stanislav@gmail.com>
2026-05-09 19:39:46 +0300

    fix(config.h): add PMW3360_CS_PIN, SPI pins, and UART serial config for RP2040 split

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,35 +1,33 @@
 #pragma once
+
+// Vial Configuration
 #define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
+// Split & Handedness
 #define MASTER_RIGHT
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
+// Pointing Device (PMW3360)
 #define POINTING_DEVICE_RIGHT
 #define POINTING_DEVICE_ROTATION_90
-
-// PMW3360 Trackball Configuration
-#define POINTING_DEVICE_DRIVER pmw3360
 #define PMW3360_CS_PIN GP14
-#define POINTING_DEVICE_CS_PIN GP14
 #define PMW3360_DPI 1600
 
-// SPI Configuration for PMW3360
+// SPI Configuration (RP2040)
 #define SPI_DRIVER SPID0
 #define SPI_SCK_PIN GP18
 #define SPI_MISO_PIN GP20
 #define SPI_MOSI_PIN GP19
 
-// Split Serial Configuration (RP2040)
-#define SERIAL_USART_TX_PIN GP1
-#define SERIAL_USART_DRIVER SD1
-#define SERIAL_USART_FULL_DUPLEX
-
-// EEPROM & Wear Leveling
-#define EEPROM_DRIVER vendor
-#define WEAR_LEVELING_ENABLE
-#define WEAR_LEVELING_RP2040_FLASH
+// EEPROM & RP2040 Specifics
 #define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
-
-// Bootloader
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+
+// Space Saving & Tuning
+#define NO_ACTION_ONESHOT
+#define TAPPING_TERM 200
