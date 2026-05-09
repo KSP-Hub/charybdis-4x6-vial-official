d5d45652caf2239cb5ebaa177659dbd41ca23435
KSP <karamin.stanislav@gmail.com>
2026-05-09 18:13:42 +0300

    fix(rules.mk): set explicit RGB_MATRIX_DRIVER=ws2812 to resolve build validation error

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
@@ -1,24 +1,18 @@
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-
-AUDIO_SUPPORTED = no        # Audio is not supported
-RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
-RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
-RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix functionality
-
-SPLIT_KEYBOARD = yes
-
-POINTING_DEVICE_ENABLE = yes # Enable trackball
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+QMK_SETTINGS = yes
+COMBO_ENABLE = yes
+ONE_SHOT_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+VIALRGB_ENABLE = no
+POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
+SPLIT_KEYBOARD = yes
+MOUSE_ENABLE = yes
+EEPROM_DRIVER = vendor
+BOOTLOADER = rp2040
 
-SERIAL_DRIVER = vendor
+# FIX: Явно указываем драйвер, чтобы пройти валидацию QMK (даже при отключенном RGB)
+RGB_MATRIX_DRIVER = ws2812
