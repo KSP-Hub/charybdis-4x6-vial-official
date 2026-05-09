41ed58967155b7836c84436837cc1020980d14f1
KSP <karamin.stanislav@gmail.com>
2026-05-09 19:07:50 +0300

    fix(keymaps/vial/config.h): add PMW3360_CS_PIN, SOFT_SERIAL_PIN and clean Vial defs

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
@@ -1,15 +1,24 @@
 #pragma once
 
-// Vial Configuration
+// Vial Security & UID
 #define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
-// Split & Pointing Device
+// Split & Master
 #define MASTER_RIGHT
 #define POINTING_DEVICE_RIGHT
 #define POINTING_DEVICE_ROTATION_90
+
+// Serial Transport (RP2040 Split)
+#define SOFT_SERIAL_PIN GP1
+#define SERIAL_USE_MULTI_TRANSACTION
+
+// PMW3360 Trackball
+#define POINTING_DEVICE_DRIVER pmw3360
+#define PMW3360_CS_PIN GP14
 #define PMW3360_DPI 1600
 
 // EEPROM (RP2040 Flash Wear Leveling)
+#define EEPROM_DRIVER vendor
 #define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
