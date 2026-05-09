a46de50d21aaf1aa2676620731b2325b5ccd99ea
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-05-09 17:48:52 +0300

    feat(vial): add keymap config.h with Vial UID, split/pointing & EEPROM settings

---
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Vial Configuration
+#define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Split & Pointing Device
+#define MASTER_RIGHT
+#define POINTING_DEVICE_RIGHT
+#define POINTING_DEVICE_ROTATION_90
+#define PMW3360_DPI 1600
+
+// EEPROM (RP2040 Flash Wear Leveling)
+#define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
