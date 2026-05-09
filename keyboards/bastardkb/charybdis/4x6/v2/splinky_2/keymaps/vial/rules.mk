9572f5704de093fd0e1dee78d8c5fd1cfff96193
KSP <karamin.stanislav@gmail.com>
2026-05-09 20:44:38 +0300

    fix: correct keymap-level rules.mk for vial (enable vial/via, disable heavy features)

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
@@ -1,8 +1,17 @@
+# Vial & VIA
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
-QMK_SETTINGS = no
-COMBO_ENABLE = no
+QMK_SETTINGS = yes
+
+# Features
+COMBO_ENABLE = yes
+ONE_SHOT_ENABLE = yes
 KEY_OVERRIDE_ENABLE = no
-SPACE_CADET_ENABLE = no
 TAP_DANCE_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+# RGB (дублируем отключение на уровне keymap для гарантии)
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+VIALRGB_ENABLE = no
