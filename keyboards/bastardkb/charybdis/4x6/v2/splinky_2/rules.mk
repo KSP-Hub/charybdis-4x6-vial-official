095290ca06f11dc72d3cc6753d294f9b13c75b4b
KSP <karamin.stanislav@gmail.com>
2026-05-09 20:43:19 +0300

    fix: remove invalid rp2040 schema fields from info.json and enforce bootloader in rules.mk

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
@@ -1,14 +1,20 @@
-BOARD = GENERIC_PROMICRO_RP2040
+# Hardware & Build Options
 BOOTLOADER = rp2040
-
-SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = vendor
 EEPROM_DRIVER = vendor
+SPLIT_KEYBOARD = yes
 
+# Pointing Device (Trackball)
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
 MOUSE_ENABLE = yes
 
+# RGB (отключаем, чтобы избежать ошибки драйвера и сэкономить память)
 RGB_MATRIX_ENABLE = no
 RGBLIGHT_ENABLE = no
 VIALRGB_ENABLE = no
+
+# Space saving (базовые отключения на уровне железа)
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+NKRO_ENABLE = no
