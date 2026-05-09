30e19fe6892450f6a1f2d2966ea949b0e336dc56
KSP <karamin.stanislav@gmail.com>
2026-05-09 20:23:17 +0300

    fix(splinky_2): define PMW3360 CS pin, SPI, serial and RP2040 reset in config.h

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,25 +1,21 @@
 #pragma once
 
-/* Vial Configuration */
-#define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-
 /* Split & Handedness */
 #define MASTER_RIGHT
-#define SOFT_SERIAL_PIN GP1
+#define SERIAL_USART_TX_PIN GP1
+#define SERIAL_USART_FULL_DUPLEX
 
-/* Pointing Device (PMW3360) */
-#define POINTING_DEVICE_RIGHT
-#define POINTING_DEVICE_ROTATION_90
-#define PMW3360_CS_PIN GP14
+/* SPI & PMW3360 Trackball */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP18
+#define SPI_MISO_PIN GP20
+#define SPI_MOSI_PIN GP19
+#define POINTING_DEVICE_CS_PIN GP14
 #define PMW3360_DPI 1600
 
-/* RP2040 Specifics */
-#define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
+/* RP2040 Bootloader */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 
-/* Tuning */
-#define NO_ACTION_ONESHOT
-#define TAPPING_TERM 200
+/* EEPROM */
+#define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
