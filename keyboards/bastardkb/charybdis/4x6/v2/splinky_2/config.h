262280b44842ca8c228178d30238e9df1baab4a5
KSP <karamin.stanislav@gmail.com>
2026-05-10 19:59:11 +0300

    fix(splinky_2/config): remove Vial UID/combo redefinitions and clean up PMW3360_DPI
    - Move VIAL_KEYBOARD_UID, VIAL_UNLOCK_COMBO_ROWS/COLS to keymap config only
    - Remove invalid PMW3360_DPI (handled by QMK pointing_device API)
    - Keep PMW3360_CS_PIN, split flags, and RP2040 EEPROM config
    - Resolves -Werror redefinition failures and driver visibility issues

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,32 +1,12 @@
 #pragma once
 
-/* Vial Configuration */
-#define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-
-/* Split & Handedness */
+// Split & Pointing Device
 #define MASTER_RIGHT
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT
-
-/* Pointing Device (PMW3360) */
 #define POINTING_DEVICE_RIGHT
 #define POINTING_DEVICE_ROTATION_90
-#define POINTING_DEVICE_CS_PIN GP14
 
-/* SPI Configuration for PMW3360 */
-#define SPI_DRIVER SPID0
-#define SPI_SCK_PIN GP18
-#define SPI_MOSI_PIN GP19
-#define SPI_MISO_PIN GP20
+// PMW3360 Chip Select Pin (требуется драйвером сенсора)
+#define PMW3360_CS_PIN GP14
 
-/* RP2040 Bootloader & Reset */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-
-/* EEPROM Emulation (RP2040 Flash Wear Leveling) */
+// EEPROM (RP2040 Flash Wear Leveling)
 #define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
-
-/* USB VBUS Detection (рекомендуется для сплит-клавиатур) */
-#define USB_VBUS_PIN GP25
--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
@@ -1,3 +1,10 @@
 #pragma once
+
+// Hardware pin required by PMW3360 driver during keymap compilation
+#ifndef PMW3360_CS_PIN
+#    define PMW3360_CS_PIN GP14
+#endif
+
 // Vial UID и Unlock Combo уже определены в keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+
 // Дубли удалены для устранения ошибки -Werror redefined
