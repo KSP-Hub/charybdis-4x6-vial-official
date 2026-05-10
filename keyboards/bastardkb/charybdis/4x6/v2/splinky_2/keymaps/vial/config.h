eb056202bd1e42fcfb2b47ea76cbe639e8e4fd0e
KSP <karamin.stanislav@gmail.com>
2026-05-10 14:39:53 +0300

    fix(vial/config.h): remove all spc

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
@@ -1,20 +1,12 @@
 #pragma once
-
-// Vial
 #define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-
-// Split & Pointing Device
 #define MASTER_RIGHT
 #define POINTING_DEVICE_RIGHT
 #define POINTING_DEVICE_ROTATION_90
-
-// PMW3360 Trackball (КРИТИЧНО ДЛЯ СБОРКИ)
 #define PMW3360_CS_PIN GP14
 #define PMW3360_DPI 1600
-
-// RP2040 Specifics
 #define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
