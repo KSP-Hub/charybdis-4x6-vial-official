29e376093d4db76b5ad16517d53c52e85638ef95
KSP <karamin.stanislav@gmail.com>
2026-05-10 13:25:53 +0300

    fix(info.json): remove processor/bootloader to bypass strict Vial-QMK schema validation

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
@@ -1,22 +1,18 @@
-# Hardware & Build Options
-BOOTLOADER = rp2040
+BOARD = GENERIC_PROMICRO_RP2040
+BOOTLOADER = custom
+
+SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = vendor
 EEPROM_DRIVER = vendor
-SPLIT_KEYBOARD = yes
 
-# Pointing Device (Trackball)
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
 MOUSE_ENABLE = yes
 
-# RGB (отключаем, чтобы избежать ошибки драйвера и сэкономить память)
 RGB_MATRIX_ENABLE = no
 RGBLIGHT_ENABLE = no
 VIALRGB_ENABLE = no
 
-# Space saving (базовые отключения на уровне железа)
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 NKRO_ENABLE = no
-BACKLIGHT_ENABLE = no
-AUDIO_ENABLE = no
