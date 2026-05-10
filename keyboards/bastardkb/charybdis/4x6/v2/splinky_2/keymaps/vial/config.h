c77c08518399bb27d379c01d81cf78f4b6bcce1f
KSP <karamin.stanislav@gmail.com>
2026-05-10 20:48:29 +0300

    1. fix(vial/keymap): align thumb cluster order with LAYOUT_charybdis_4x6 macro (3L+2R / 2L+1R) 2. fix(vial/rules.mk): remove conflicting BOOTLOADER definition, clean up keymap-level features 3. fix(vial/config.h): isolate keymap-level Vial & pointing device overrides, remove hardware duplicates

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
@@ -1,10 +1,23 @@
 #pragma once
 
-// Hardware pin required by PMW3360 driver during keymap compilation
-#ifndef PMW3360_CS_PIN
-#    define PMW3360_CS_PIN GP14
-#endif
+// Vial Configuration
+#define VIAL_KEYBOARD_UID {0x04, 0x03, 0x02, 0x01, 0x69, 0x42, 0xEF, 0xBE}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
-// Vial UID и Unlock Combo уже определены в keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+// Split & Pointing Device (Keymap overrides)
+#define MASTER_RIGHT
+#define POINTING_DEVICE_RIGHT
+#define POINTING_DEVICE_ROTATION_90
 
-// Дубли удалены для устранения ошибки -Werror redefined
+// DPI Settings
+#define PMW3360_DPI 1600
+#define PMW3360_CS_PIN GP14
+
+// EEPROM (RP2040 Flash Wear Leveling)
+#define RP2040_FLASH_WEAR_LEVELING_SIZE 16384
+
+// Tapping & Modifiers
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/rules.mk
@@ -5,13 +5,20 @@ VIAL_INSECURE = yes
 QMK_SETTINGS = yes
 
 # Features
-COMBO_ENABLE = yes
-ONE_SHOT_ENABLE = yes
+COMBO_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
-TAP_DANCE_ENABLE = no
 SPACE_CADET_ENABLE = no
+TAP_DANCE_ENABLE = no
+MOUSEKEY_ENABLE = yes
 
-# RGB (дублируем отключение на уровне keymap для гарантии)
+# Pointing Device (наследуется от keyboard level, но явно разрешаем)
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3360
+
+# RGB (отключаем для экономии flash и избежания ошибок схемы Vial)
 RGB_MATRIX_ENABLE = no
 RGBLIGHT_ENABLE = no
 VIALRGB_ENABLE = no
+
+# Bootloader handled by info.json to avoid CI schema conflict
+# BOOTLOADER = custom
