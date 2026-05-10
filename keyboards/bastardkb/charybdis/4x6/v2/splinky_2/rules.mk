d7e30bb763a79f5575ffc3e0e4a286db0b0edc2c
KSP <karamin.stanislav@gmail.com>
2026-05-10 18:39:22 +0300

    1. fix(splinky_2/rules.mk): align RP2040 bootloader, EEPROM wear-leveling and RGB driver with official vial-qmk baseline 2. fix(vial/vial.json): remove invalid processor/bootloader fields to pass QMK CI schema validation 3. fix(splinky_2/info.json): clean up keys/values and remove conflicting bootloader/processor definitions 4. fix(splinky_2/config.h): sync with official vial-qmk splinky_2 baseline, add RP2040 reset timeout and Vial UID

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
@@ -1,18 +1,31 @@
-BOARD = GENERIC_PROMICRO_RP2040
+# Core Features
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+QMK_SETTINGS = no
+
+# Hardware & Drivers (RP2040 Official Vial-QMK baseline)
+MCU = RP2040
 BOOTLOADER = rp2040
+SERIAL_DRIVER = vendor
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_PROVIDER = rp2040_flash
 
 SPLIT_KEYBOARD = yes
-SERIAL_DRIVER = vendor
-EEPROM_DRIVER = vendor
 
+# Pointing Device
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
 MOUSE_ENABLE = yes
 
+# RGB (Explicitly disabled, but driver set to pass QMK schema validation)
 RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = ws2812
 RGBLIGHT_ENABLE = no
 VIALRGB_ENABLE = no
 
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-NKRO_ENABLE = no
+# Space Saving
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+SPACE_CADET_ENABLE = no
+TAP_DANCE_ENABLE = no
